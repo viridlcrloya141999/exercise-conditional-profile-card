a29d985f279d1231b082bf08278a365094721c9c
Viridiana De la cruz <167374369+viridlcrloya141999@users.noreply.github.com>
2024-06-15 06:24:50 +0000

    intento2

--- src/js/app.js
+++ src/js/app.js
@@ -33,19 +33,28 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1> 
-           ${variables.name == null ? "name" : variables.name}
+
+           <h1> 
+          ${variables.name == null ? "name" : variables.name}
            ${variables.lastName == null ? "last name" : variables.lastName} 
-          </h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+           </h1>
+           
+            <h2>
+  document.querySelector("#widget_h2").innerHTML = `< div class="widget" >
+    ${ h2 } 
+            </h2 >
+
+           <h3>${variables.city}, ${variables.country}</h3>
+          
+          // <h3>Miami, USA</h3>
+        <ul class="position-right">
+          <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+          <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
-        </div>
+
+        </div >
     `;
 }
 
