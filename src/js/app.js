f44edb9c6226ebde71eb7b65df90fe6f84d18641
Viridiana De la cruz <167374369+viridlcrloya141999@users.noreply.github.com>
2024-06-20 00:13:59 +0000

    entrega 2

--- src/js/app.js
+++ src/js/app.js
@@ -44,11 +44,19 @@ function render(variables = {}) {
     variables.country == null ? "country" : variables.country
   }</h3>
 
-            <ul class="position-right">
-                <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-                <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-                <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <ul class="${variables.socialMediaPosition}">
+                <li><a href="https://twitter.com/${
+                  variables.twitter
+                }"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://github.com/${
+                  variables.github
+                }"><i class="fab fa-github"></i></a></li>
+                <li><a href="https://linkedin.com/school/${
+                  variables.linkedin
+                }"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="https://instagram.com/${
+                  variables.instagram
+                }"><i class="fab fa-instagram"></i></a></li>
             </ul>
 
         </div >
